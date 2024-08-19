9db23658e37d8d9eac9e000ec44c5badbaa92b26
super-ykp <163214722+super-ykp@users.noreply.github.com>
2024-08-19 10:49:48 +0900

    Update camera.go

--- yoko/camera/camera.go
+++ yoko/camera/camera.go
@@ -6,18 +6,21 @@ import (
 )
 
 var (
+	//カメラ座標
 	X int
 	Y int
-
+	//カメラが実際に映すズレ
 	CamOffsetX int
 	CamOffsetY int
-
+	//振動
 	Vibration float64
 )
 
 func Update() {
+	//カメラは基本カメラ座標に追従する
 	CamOffsetX = X - common.SCREEN_WIDTH/2
 	CamOffsetY = Y + common.CAM_Y_OFFSET
+	//振動エフェクト発生中は、オフセットにランダム値が加算される
 	if Vibration > 0 {
 		CamOffsetX += int(rand.Float64()*Vibration*2 - Vibration)
 		CamOffsetY += int(rand.Float64()*Vibration*2 - Vibration)
@@ -25,7 +28,7 @@ func Update() {
 	}
 
 }
-
+//カメラ振動
 func SetViblation(v float64) {
 	Vibration = v
 }
